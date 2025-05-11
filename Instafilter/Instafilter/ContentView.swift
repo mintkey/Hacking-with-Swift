e051490dc9219ad718d601476b3c1aff14b9211d
mintkey <40031593+mintkey@users.noreply.github.com>
2025-05-11 19:23:39 -0400

    Move files

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,31 +5,11 @@
 //  Created by Helen Dempsey on 5/11/25.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .onAppear(perform: loadImage)
-    }
-
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+        Text("Hello, world!")
     }
 }
 
